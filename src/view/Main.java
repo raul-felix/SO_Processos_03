4f7981d0725e62593cb8ad1a6b8da5c9a4a06aed
Raul Felix <raul.sousa2@fatec.sp.gov.br>
2024-08-25 16:01:21 -0300

    Update Main.java

--- src/view/Main.java
+++ src/view/Main.java
@@ -1,5 +1,13 @@
 package view;
 
+import controller.DistroController;
+
 public class Main {
+	
+	public static void main(String[] args) {
+		DistroController distroController = new DistroController();
+		
+		distroController.exibeDistro();
+	}
 
 }
